8c017326edb6f43171e468c93264e62352391af4
Stefan Weck <stefanweck1@gmail.com>
2014-02-13 00:16:26 +0100

    Added a debug function that highlights rooms and logs info about the current tile.

--- JS/init.js
+++ JS/init.js
@@ -21,13 +21,11 @@ function initializeCanvas(){
     oCanvas.width = 900;
     oCanvas.height = 600;
 
-    //Set click event to the canvas
-    oCanvas.addEventListener('click', function(){
-        dungeon = new Dungeon(60, 40, 10);
-    }, false);
+    //Create a new game
+    var game = new Roguelike.Game();
 
-    //Create a new dungeon
-    var dungeon = new Dungeon(
+    //Initialize the game
+    game.initialize(
         //Tiles X
         60,
         //Tiles Y
@@ -35,5 +33,28 @@ function initializeCanvas(){
         //Maximum number of rooms
         10
     );
+
+    //Debug functionality
+    canvas.addEventListener('mousemove', function(e) {
+        var mousePos = Roguelike.Utils.getMousePos(canvas, e);
+        var x = Math.floor(mousePos.x / 15 );
+        var y = Math.floor(mousePos.y / 15 );
+
+        console.clear();
+        game.renderer.draw(game.map);
+        tileType = game.map.tiles[y][x].type;
+
+        console.log(game.map.tiles[y][x]);
+
+        oContext.fillStyle = "rgba(255, 0, 0, 0.2)";
+
+        if(game.map.tiles[y][x].belongsTo){
+            var room = game.map.tiles[y][x].belongsTo;
+            oContext.fillRect(room.x1 * game.map.tileSize, room.y1 * game.map.tileSize , room.w * game.map.tileSize, room.h * game.map.tileSize);
+        }else{
+            oContext.fillRect(x * game.map.tileSize, y * game.map.tileSize , game.map.tileSize, game.map.tileSize);
+        }
+
+    }, false);
 
 }
