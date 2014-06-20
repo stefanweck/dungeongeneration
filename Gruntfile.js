13bf9e6c029777ecc8d1c1143536bf3ab54412f7
Stefan Weck <stefanweck1@gmail.com>
2014-06-20 21:04:58 +0200

    Starting with a new blank Gruntfile.js.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,89 +1,20 @@
-/*global module:false*/
+'use strict';
+
 module.exports = function(grunt) {
 
-	// Project configuration.
+	// configure grunt
 	grunt.initConfig({
-		// Metadata.
+
 		pkg: grunt.file.readJSON('package.json'),
-		banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-			'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-			'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-			'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %> */\n',
-		// Task configuration.
-		concat: {
-			options: {
-				banner: '<%= banner %>',
-				stripBanners: true
-			},
-			dist: {
-				src: ["lib/roguelike.js",
-					"lib/core/game.js",
-					"lib/core/camera.js",
-					"lib/core/textlog.js",
-					"lib/geometry/vector2.js",
-					"lib/geometry/boundary.js",
-					"lib/gameobjects/entity.js",
-					"lib/gameobjects/group.js",
-					"lib/gameobjects/components/sprite.js",
-					"lib/gameobjects/components/health.js",
-					"lib/gameobjects/components/canopen.js",
-					"lib/gameobjects/components/lightsource.js",
-					"lib/gameobjects/components/collide.js",
-					"lib/gameobjects/components/tooltip.js",
-					"lib/gameobjects/components/keyboardcontrol.js",
-					"lib/gameobjects/components/position.js",
-					"lib/gameobjects/components/weapon.js",
-					"lib/gameobjects/components/canfight.js",
-					"lib/gameobjects/components/movement.js",
-					"lib/gameobjects/systems/render.js",
-					"lib/gameobjects/systems/open.js",
-					"lib/gameobjects/systems/lightmap.js",
-					"lib/gameobjects/systems/movement.js",
-					"lib/gameobjects/systems/combat.js",
-					"lib/gameobjects/systems/pathfinding.js",
-					"lib/gameobjects/behaviours/moveBehaviours.js",
-					"lib/factories/playerfactory.js",
-					"lib/factories/enemyfactory.js",
-					"lib/factories/propfactory.js",
-					"lib/factories/decorationfactory.js",
-					"lib/input/event.js",
-					"lib/input/key.js",
-					"lib/input/keyboard.js",
-					"lib/time/queue.js",
-					"lib/time/scheduler.js",
-					"lib/ui/element.js",
-					"lib/ui/container.js",
-					"lib/ui/elements/image.js",
-					"lib/ui/elements/text.js",
-					"lib/tilemap/tile.js",
-					"lib/tilemap/map.js",
-					"lib/tilemap/mapfactory.js",
-					"lib/tilemap/mapdecorator.js",
-					"lib/tilemap/room.js",
-					"lib/libraries/easystar.js",
-					"lib/libraries/mersennetwister.js",
-					"lib/core/utils.js",
-					"lib/init.js"
-				],
-				dest: 'dist/<%= pkg.name %>.js'
-			}
-		},
-		uglify: {
-			options: {
-				banner: '<%= banner %>'
-			},
-			dist: {
-				src: '<%= concat.dist.dest %>',
-				dest: 'dist/<%= pkg.name %>.min.js'
-			}
-		}
+
 	});
 
-	// These plugins provide necessary tasks.
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+	// Load plug-ins
+	// grunt.loadNpmTasks('grunt-contrib-whatever');
 
-	// Default task.
-	grunt.registerTask('default', ['concat', 'uglify']);
-
+	// define tasks
+	grunt.registerTask('default', [
+		// No tasks, yet
+	]);
+	
 };
